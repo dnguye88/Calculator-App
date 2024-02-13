ab96c3e3bdb787726e992125214b7e29186b667a
Duy Nguyen <zzohfefezz588@gmail.com>
2024-02-13 16:55:16 -0500

    finished
    added delete function

---
+++ script.js
@@ -0,0 +1,25 @@
+//Calculator Functions
+
+const display = document.getElementById("display");
+const currentNumber = " ";
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+//Delete the last value input into the display
+function deleteValue() {
+  display.value = display.value.slice(0, -1);
+}
